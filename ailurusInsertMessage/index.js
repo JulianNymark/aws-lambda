72d0292fb94fd89718c04db325c42ef9e2c64963
Julian Nymark <j@Julians-MacBook-Air.local>
2022-07-27 15:41:44 +0200

    add loging ip and geolocation of ip

--- ailurusInsertMessage/index.js
+++ ailurusInsertMessage/index.js
@@ -1,35 +1,44 @@
 require("dotenv").config();
+const axios = require('axios')
 const MongoClient = require("mongodb").MongoClient;
+
+const apikey_ipgeolocation = process.env['API_KEY_IPGEOLOCATION']
+const uri = process.env["DB_URL"];
 
 let client;
 
 exports.handler = async (event, context, callback) => {
   const { name = "", email = "", message = "" } = await JSON.parse(event.body);
 
-  const uri = process.env["DB_URL"];
+  const sourceIp = event.requestContext.http.sourceIp
+  const userAgent = event.requestContext.http.userAgent
+  const sourceIpGeo = await sourceIpLookup(sourceIp)
+
   client = new MongoClient(uri);
 
   let result = await addMessage(
-    name.substr(0, 200),
-    email.substr(0, 200),
-    message.substr(0, 2000)
+    {
+      name: name.substr(0, 200),
+      email: email.substr(0, 200),
+      message: message.substr(0, 2000),
+      sourceIp,
+      sourceIpGeo,
+      userAgent
+    }
   );
+
   client.close(); // be nice to the db
 
   return result;
 };
 
-const addMessage = async (name, email, message) => {
+const addMessage = async (data) => {
   try {
     await client.connect();
     const messages = await client
       .db("ailurus_messages")
       .collection("messages")
-      .insertOne({
-        name,
-        email,
-        message,
-      });
+      .insertOne(data);
     console.log(messages);
     return {
       statusCode: 200,
@@ -65,3 +74,19 @@ const getMessages = async () => {
     };
   }
 };
+
+const sourceIpLookup = async (ip) => {
+  const ripeLookupReq = axios(`https://rdap.db.ripe.net/ip/${ip}`)
+  const ipGeoLocationReq = axios(`https://api.ipgeolocation.io/ipgeo?apiKey=${apikey_ipgeolocation}&ip=${ip}`)
+
+  const [ripeLookup, ipGeoLocation] = await Promise.all([ripeLookupReq, ipGeoLocationReq])
+
+  return {
+    country: ripeLookup?.data?.country || null,
+    name: ripeLookup?.data?.name || null,
+    location: {
+      lat: ipGeoLocation?.data?.latitude || null,
+      lon: ipGeoLocation?.data?.longitude || null,
+    }
+  }
+}
