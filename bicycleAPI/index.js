45085af05e86f9ee54ef405e43f1e0f5240418bb
Julian Nymark <j@juliannymark.com>
2023-01-22 11:49:05 +0100

    add npm script for local development

--- bicycleAPI/index.js
+++ bicycleAPI/index.js
@@ -1,8 +1,10 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const serverless = require('serverless-http');
+const Koa = require("koa")
+const Router = require("koa-router")
+const serverless = require("serverless-http")
 
-const { getStations, getStation } = require('./db');
+const { getStations, getStation } = require("./db")
+
+const environment = process.env.NODE_ENV ?? "serverless"
 
 const app = new Koa()
 const router = new Router()
@@ -15,19 +17,24 @@ router
     const requestBody = ctx.request
     ctx.body = requestBody
   })
-  .get('/api/v1/station', async (ctx) => {
+  .get("/api/v1/station", async (ctx) => {
     ctx.body = await getStations()
   })
-  .get('/api/v1/station/:station_id', async (ctx) => {
+  .get("/api/v1/station/:station_id", async (ctx) => {
     const id = ctx.params.station_id
     ctx.body = await getStation(id)
   })
-  .get('/api/v1/station/nearby/:location/:radius', (ctx) => {
+  .get("/api/v1/station/nearby/:location/:radius", (ctx) => {
     const location = ctx.params.location
     const radius = ctx.params.radius
-    ctx.body = `NOT IMPLEMENTED: all the stations within ${radius} of ${location}`
+    ctx.body = `NOT IMPLEMENTED: all the stations within ${radius} meters of ${location}`
   })
 
 app.use(router.routes()).use(router.allowedMethods())
 
-module.exports.handler = serverless(app)
+if (environment === "serverless") {
+  module.exports.handler = serverless(app)
+} else {
+  console.debug("starting in development mode...")
+  app.listen(3000)
+}
